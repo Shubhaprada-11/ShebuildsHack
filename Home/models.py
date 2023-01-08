e351db43a53377619566571a72223ed2f94c5488
Shubhaprada-11 <shubhapradakp.cs20@rvce.edu.in>
2023-01-08 14:12:34 +0530

    new first

--- Home/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class UserPeriod(models.Model):
-    uid = models.ForeignKey(User,related_name='user_id',on_delete=models.CASCADE)
-    lasperioddate = models.DateField('%Y-%m-%d')
-    duration = models.IntegerField()
